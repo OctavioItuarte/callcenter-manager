cdb09e61105ca7cdd3516707673e58cd7c173097
Lucas Quiroga <quiroga9409@gmail.com>
2024-04-12 15:48:43 -0300

    Agrego ruta post en el controller

--- src/main/java/app/controller/FileController.java
+++ src/main/java/app/controller/FileController.java
@@ -4,6 +4,7 @@ import app.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class FileController {
     public final static String FILE_UPLOAD_PATH = "src/main/resources/static/";
 
 
+    @PostMapping("")
     public ResponseEntity<?> uploadFile(@RequestParam("csvFile") MultipartFile file) {
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().body("Archivo no seleccionado");
@@ -30,7 +32,8 @@ public class FileController {
             fileService.fileRender(file.getName());
             return ResponseEntity.ok("Archivo CSV recibido y procesado correctamente");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al procesar la imagen");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al procesar el archivo");
         }
     }
 }
