bc1498320fc036bada6b5dcc2484a7b33b4e72b1
Lucas Quiroga <quiroga9409@gmail.com>
2024-04-16 02:36:01 -0300

    Puse la notacion Table para que el ORM no confunda User con un comando sql

--- src/main/java/app/domain/User.java
+++ src/main/java/app/domain/User.java
@@ -1,13 +1,11 @@
 package app.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import javax.validation.constraints.Email;
 
 @Entity
+@Table(name = "`user`")
 public class User {
 
     @Id
