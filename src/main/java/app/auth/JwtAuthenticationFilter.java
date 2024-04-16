d9c6732094ccbacd416ed71557fcbafbe2b3f368
Agustin Carretto <etemagus@gmail.com>
2024-04-16 18:36:55 -0300

    El metodo doFilterInternal ahora filtra el rol del usuario

--- src/main/java/app/auth/JwtAuthenticationFilter.java
+++ src/main/java/app/auth/JwtAuthenticationFilter.java
@@ -5,12 +5,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -28,7 +30,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             String token = getTokenFromRequest(request);
             if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
                 String nombreUsuario = jwtTokenProvider.getNameUserFromToken(token);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(nombreUsuario, null, null);
+                String rolUsuario = jwtTokenProvider.getRoleFromToken(token);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(nombreUsuario, null, Collections.singleton(new SimpleGrantedAuthority(rolUsuario)));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
@@ -36,6 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
+
     }
 
     private String getTokenFromRequest(HttpServletRequest request) {
