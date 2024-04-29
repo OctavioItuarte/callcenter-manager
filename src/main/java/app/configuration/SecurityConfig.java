b701ecfa2c5a3b4410343f68ec5c21840e888372
Lucas Quiroga <quiroga9409@gmail.com>
2024-04-29 07:32:49 -0300

    Arreglo problema de tokens expirados que generaban un bloqueo en los endpoints

--- src/main/java/app/configuration/SecurityConfig.java
+++ src/main/java/app/configuration/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -47,10 +48,11 @@ public class SecurityConfig  {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
-
     }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
