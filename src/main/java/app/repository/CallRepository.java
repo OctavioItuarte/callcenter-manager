527a761e36407705637ad942336c7ef4013c220b
Lucas Quiroga <quiroga9409@gmail.com>
2024-05-04 16:43:25 -0300

    Se crea query para filtrar resultados en la base de datos

--- src/main/java/app/repository/CallRepository.java
+++ src/main/java/app/repository/CallRepository.java
@@ -2,14 +2,17 @@ package app.repository;
 
 import app.domain.Call;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface CallRepository extends JpaRepository<Call, Long> {
 
     List<Call> findByFileId(Long fileId);
-
-    List<Call> findBySourceTrunkOrderByDestinationTrunk(String trunk);
+    @Query(value = "SELECT * FROM call c WHERE (c.source_trunk = :trunk OR c.destination_trunk = :trunk) AND c.caller LIKE ANY (ARRAY[:start])", nativeQuery = true)
+    List<Call> findByTrunkAndCallerStartingWith(@Param("trunk") String trunk, @Param("start") String[] start);
 }
