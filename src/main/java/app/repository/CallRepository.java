befdfe5c124c24b8ea065252be541b5677f5297b
Lucas Quiroga <quiroga9409@gmail.com>
2024-05-06 16:11:23 -0300

    Elimine importacion innecesaria

--- src/main/java/app/repository/CallRepository.java
+++ src/main/java/app/repository/CallRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
+
 
 @Repository
 public interface CallRepository extends JpaRepository<Call, Long> {
